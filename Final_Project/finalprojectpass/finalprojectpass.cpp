ca50f0f070d89343f4e01853d166b590321ff3e0
Lindsey Forche <lforche@umich.edu>
2022-11-24 18:39:55 -0500

    Separated isMustAlias and isNoAlias

--- Final_Project/finalprojectpass/finalprojectpass.cpp
+++ Final_Project/finalprojectpass/finalprojectpass.cpp
@@ -31,7 +31,9 @@ namespace{
                         }
                         else if (aliasedInst.size() == 1 && isa<LoadInst>((&(*(i)))->getNextNode())) {
                             errs() << *((*i).getNextNode()) << "\t" << *(i->getNextNode()->getOperand(0)) << "\n";
-                            if(AA.isMustAlias(i->getNextNode()->getOperand(0), aliasedInst[0]->getOperand(1)) && AA.isNoAlias(i->getNextNode()->getOperand(0), aliasedInst[0]->getOperand(1)))
+                            if (AA.isMustAlias(i->getNextNode()->getOperand(0), aliasedInst[0]->getOperand(1)))
+                                errs() << "yes" << "\n";
+                            else if (AA.isNoAlias(i->getNextNode()->getOperand(0), aliasedInst[0]->getOperand(1)))
                                 errs() << "nah" << "\n";
                             else
                                 errs() << "maybe" << "\n"; 
