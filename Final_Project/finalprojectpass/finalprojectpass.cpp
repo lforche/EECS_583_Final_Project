fbd08cac82eefc7694f0458c8369bf0bb8ef9aeb
Lindsey Forche <lforche@umich.edu>
2022-11-24 14:18:54 -0500

    Looked at operands that should be different

--- Final_Project/finalprojectpass/finalprojectpass.cpp
+++ Final_Project/finalprojectpass/finalprojectpass.cpp
@@ -5,6 +5,7 @@
 #include "llvm/Support/Format.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/InstrTypes.h"
 #include <iostream>
 
 using namespace std;
@@ -17,19 +18,35 @@ namespace{
         }
 
 		virtual bool runOnFunction(Function &F) override{
-            // set<Instruction> arrRefInst;
-            // AliasAnalysis *AA = &getAnalysis<AAResultsWrapperPass>().getAAResults();
+            vector<Instruction*> aliasedInst;
             for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
                 for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
                     // if ((&(*(i)))->getNextNode() != nullptr
                     // && (&(*(i)))->getOpcode() == Instruction::Store && ((&(*(i)))->getNextNode())->getOpcode() == Instruction::Load)
                     // if ((*i).getNumOperands()) {
-                    // if (isa<FunctionType>(*i)){
-                        errs() << *i << "\t" << (i->getFunction()->getName()) << "\n";
-                        if (i->getFunction()->getName() == "myFunc") {
-                            for(auto arg = i->getFunction()->arg_begin(); arg != i->getFunction()->arg_end(); arg++)
-                                errs() << *arg << "\n";
+                    if (isa<GetElementPtrInst>(i)) {
+                        if (aliasedInst.size() == 0 && isa<StoreInst>((&(*(i)))->getNextNode())) {
+                            errs() << *((*i).getNextNode()) << "\t" << (i->getNextNode()->getOperand(1)) << "\n";
+                            aliasedInst.push_back(&(*i));
                         }
+                        else if (aliasedInst.size() == 1 && isa<LoadInst>((&(*(i)))->getNextNode())) {
+                            errs() << *((*i).getNextNode()) << "\t" << (i->getNextNode()->getOperand(0)) << "\n";
+                            // if ((i->getOperand(0) == aliasedInst[0]->getOperand(0)) && (i->getOperand(1) == aliasedInst[0]->getOperand(1)))
+                            //     errs() << "ahh\n"; 
+                            aliasedInst.push_back(&(*i));
+                        }
+                        // if (i->getFunction()->getName() == "main" && isa<CallInst>(i)) {
+                        //     errs() << "\n------1-" << *i << "\t" << (i->getFunction()->getName()) << "------\n";
+                        //     for(auto arg = dyn_cast<CallInst>(i)->getCalledFunction()->arg_begin(); arg != dyn_cast<CallInst>(i)->getCalledFunction()->arg_end(); arg++) {
+                        //         for (auto it : (arg)->users()) {
+                        //             if (isa<StoreInst>(it)) {
+                        //                 arrParems.push_back(it->getOperand(1));
+                        //                 errs() << *(it->getOperand(1)) << "\n";
+                        //             }
+                        //         }
+                        //     }
+                        //     errs() << "\n------2-" << dyn_cast<CallInst>(i)->getCalledFunction()->getName() << "------\n";
+                        // }
 
                         // for (auto it : ((*i).getOperand(0))->users()) {
                         //     if (isa<GetElementPtrInst>(*i)) {
@@ -42,7 +59,7 @@ namespace{
                         // errs() << (&(*(i)))->getOpcodeName() << "\n";
                         // errs() << (&(*(i)))->getOpcodeName() << "\t" << ((&(*(i)))->getNextNode())->getOpcodeName() << "\n";
                         // errs() << (*i) << "\t" << (*(&(*(i)))->getNextNode()) << "---" << dyn_cast<StoreInst>(&(*(i)))->getPointerOperand()->getName() << "\t" << ((&(*(i)))->getNextNode())->getOperand(0)->getName() << "\n";
-                    // }
+                    }
                 }
             }
 			return false; 
