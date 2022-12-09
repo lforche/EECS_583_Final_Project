d763db781bd77ef32cbce16fdc614efc85e19257
Lindsey Forche <lforche@umich.edu>
2022-12-09 02:44:02 -0500

    updated run.sh files and found loop values in main

--- Final_Project/finalprojectpass/finalprojectpass.cpp
+++ Final_Project/finalprojectpass/finalprojectpass.cpp
@@ -19,7 +19,8 @@
 
 using namespace std;
 using namespace llvm;
-namespace{
+
+namespace Performance{
 	struct FinalProject: public FunctionPass {
         static char ID;
 
@@ -48,24 +49,54 @@ namespace{
                     // get loop
                     Loop* L = LI.getLoopFor(&(*bb));
                     
-                    // auto loopBounds = L->getBounds(SE);
-                    
-                    // if (L != NULL && loopBounds)
-                    //     errs() << "a\n";
-                    // errs() 
-                    // if (L != NULL && loopBounds->getStepValue() != nullptr)
-                    //     errs() << *(L->getBounds(SE)->getStepValue()) << "\n";
-                    // auto loopInit = loopBounds.getPointer()->getInitialIVValue();
-                    // errs() << L->getNumBlocks() << "\n";
+                    if (L && L->getInductionVariable(SE)) {
+                        Optional<Loop::LoopBounds> loopBounds = Loop::LoopBounds::getBounds(*L, *(L->getInductionVariable(SE)), SE);
+                        if (loopBounds) {
+                            // gets start index
+                            ConstantInt* startVar = dyn_cast<ConstantInt>(&loopBounds->getInitialIVValue()); 
+                            
+                            // gets end index and can maybe use?
+                            // ConstantInt* endVar = dyn_cast<ConstantInt>(&loopBounds->getFinalIVValue());  
+                            
+                            // get tripCount and can maybe use?                        
+                            // unsigned int tripCount = SE.getSmallConstantTripCount(L);
+                            
+                            // gets M variable instruction -- use since end index and tripCount don't work
+                            ICmpInst* endVarInst = L->getLatchCmpInst(); 
+
+                            // gets M variable operand -- wide exit count
+                            Value* endVarOp = endVarInst->getOperand(1); 
+
+                            if (startVar && endVarOp) {
+                                errs() << startVar->getValue() << "\t" << *endVarOp << "\n";
+                            }
+                        }
+                    }
                 }
-                errs() << "\n\n";
             }
 			return true; 
 		}
 	};
 }
 
-char FinalProject::ID = 0;
-static RegisterPass<FinalProject> X("finalproject", "Final Project pass",
+char Performance::FinalProject::ID = 0;
+static RegisterPass<Performance::FinalProject> X("finalproject-performance", "Final Project Performance pass",
+    false /* Only looks at CFG */,
+    false /* Analysis Pass */);
+
+namespace Control {
+	struct FinalProject: public FunctionPass {
+        static char ID;
+
+		FinalProject() : FunctionPass(ID) {}
+
+		virtual bool runOnFunction(Function &F) override{
+            return true; 
+		}
+	};
+}
+
+char Control::FinalProject::ID = 0;
+static RegisterPass<Control::FinalProject> Y("finalproject-control", "Final Project Control pass",
     false /* Only looks at CFG */,
     false /* Analysis Pass */);
