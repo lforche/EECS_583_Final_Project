072dd5000013ed72a8ede4e8467edf575681bf11
zderosia <zach.derosia@hotmail.com>
2022-12-09 00:44:57 -0500

    Added Example6, which successfully breaks LLVM loop versioning. Also added walk.sh, an unoptimized version of run.sh. It can be used to compile an unoptimized version of the desired benchmark.

--- Final_Project/walk.sh
+++ Final_Project/walk.sh
@@ -36,29 +36,29 @@ cleanup
 setup
 
 # # Convert source code to bitcode (IR)
-# clang -emit-llvm -c ${1}.c -o ${1}.bc
+clang -emit-llvm -c ${1}.c -o ${1}.bc
 # # Canonicalize natural loops
-# opt -enable-new-pm=0 -loop-simplify ${1}.bc -o ${1}.ls.bc
+opt -enable-new-pm=0 -loop-simplify ${1}.bc -o ${1}.ls.bc
 # # Instrument profiler
-# opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
+opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
 # # Generate binary executable with profiler embedded
-# clang -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}.prof
+clang -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}.prof
 
 # Convert source code to bitcode (IR)
-clang -O2 -emit-llvm -c ${1}.c -o ${1}.bc
+# clang -O2 -emit-llvm -c ${1}.c -o ${1}.bc
 # Canonicalize natural loops
-opt -O2 -enable-new-pm=0 -loop-simplify ${1}.bc -o ${1}.ls.bc
+# opt -O2 -enable-new-pm=0 -loop-simplify ${1}.bc -o ${1}.ls.bc
 # Instrument profiler
-opt -O2 -enable-new-pm=0 -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
+# opt -O2 -enable-new-pm=0 -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
 # Generate binary executable with profiler embedded
-clang -O2 -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}.prof
+# clang -O2 -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}.prof
 
 # Generate profiled data
-# ./${1}.prof ${INPUT}
-# llvm-profdata merge -o ${1}.profdata default.profraw
+./${1}.prof ${INPUT}
+llvm-profdata merge -o ${1}.profdata default.profraw
 
 # Prepare input to run
 setup
 # Apply your pass to bitcode (IR)
-opt -enable-new-pm=0 -load ${PATH_MYPASS} ${NAME_MYPASS} < ${1}.bc > /dev/null
-# opt -enable-new-pm=0 -pgo-instr-use -load ${PATH_MYPASS} ${NAME_MYPASS} < ${1}.bc > /dev/null
+# opt -enable-new-pm=0 -load ${PATH_MYPASS} ${NAME_MYPASS} < ${1}.bc > /dev/null
+opt -enable-new-pm=0 -pgo-instr-use -load ${PATH_MYPASS} ${NAME_MYPASS} < ${1}.bc > /dev/null
